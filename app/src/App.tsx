6429045d4e3ca4d67cdafc4ed625921e7f1e76b7
Daria Malina <gluto68@gmail.com>
2022-01-15 14:56:29 +0300

    feat: add hoock

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [type, setType] = useState('user');
+  const [data, setData] = useState([]);
+  const [pos, setPos] = useState({
+    x: 0,
+    y: 0,
+  });
+  const mouseMoveHandler = (ev) => {
+    setPos({ x: ev.clientX, y: ev.clientY });
+  };
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${type}`)
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, [type]);
+
+  useEffect(() => {
+    window.addEventListener('mousemove', mouseMoveHandler);
+    return () => {
+      window.removeEventListener('mousemove', mouseMoveHandler);
+    };
+  }, []);
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        Ресурс:
+        {type}
+      </h1>
+      <button type="button" onClick={() => { setType('users'); }}>Пользователи</button>
+      <button type="button" onClick={() => { setType('todos'); }}>Todo</button>
+      <button type="button" onClick={() => { setType('posts'); }}>Посты</button>
+      {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+      <pre>{JSON.stringify(pos, null, 2)}</pre>
+
     </div>
   );
 }
